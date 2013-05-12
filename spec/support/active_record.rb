af948ef7b353097a8054d85a3946bd56d636655c
Ben Zimmer <benjamin.zimmer@infopark.de>
2013-05-12 14:17:26 +0200

    specs: only run migrations if necessary

--- spec/support/active_record.rb
+++ spec/support/active_record.rb
@@ -1,7 +1,20 @@
 require 'active_record'
 
 ActiveRecord::Base.establish_connection adapter: "sqlite3", database: "db/stringer_test.sqlite"
-ActiveRecord::Migrator.up "db/migrate"
+
+def need_to_migrate?
+  current_migration = ActiveRecord::Migrator.current_version
+  latest_migration = begin
+    ActiveRecord::Migrator.get_all_versions.last
+  rescue ActiveRecord::StatementInvalid
+    nil
+  end
+  current_migration != latest_migration
+end
+
+if need_to_migrate?
+  ActiveRecord::Migrator.up "db/migrate"
+end
 
 RSpec.configure do |config|
   config.around do |example|
