783a3d506d7aa87aca660e56454a6c77942723cd
Matt Swanson <mdswanson@sep.com>
2013-05-14 22:16:14 -0400

    fix asset-pack to serve assets from public dir, instead of through the app, closes #112

--- app.rb
+++ app.rb
@@ -53,9 +53,9 @@ class Stringer < Sinatra::Base
   end
 
   assets {
-    serve "/js",     from: "app/public/js"
-    serve "/css",    from: "app/public/css"
-    serve "/images", from: "app/public/img"
+    serve "/js",     from: "/js"
+    serve "/css",    from: "/css"
+    serve "/images", from: "/img"
 
     js :application, "/js/application.js", [
       "/js/jquery-min.js",
