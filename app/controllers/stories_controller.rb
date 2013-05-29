9b96206afb0e8cdc4ca143dba1f6164d43656c4d
Matt Swanson <mdswanson@sep.com>
2013-05-28 23:10:02 -0400

    Move mark_all_as_read to under stories resource.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -31,7 +31,7 @@ class Stringer < Sinatra::Base
     StoryRepository.save(story)
   end
 
-  post "/mark_all_as_read" do
+  post "/stories/mark_all_as_read" do
     MarkAllAsRead.new(params[:story_ids]).mark_as_read
     
     redirect to("/news")
