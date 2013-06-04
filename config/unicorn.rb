65049bf1e72f8d48f1a8c21eb162f218c2f6959d
Jiehan Zheng <zheng@jiehan.org>
2013-06-04 19:34:36 -0400

    third try

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,11 +5,25 @@ preload_app true
 @delayed_job_pid = nil
 
 before_fork do |server, worker|
+  # the following is highly recommended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+
   @delayed_job_pid ||= spawn("bundle exec rake work_jobs")
 
   sleep 1
 end
 
 after_fork do |server, worker|
-  ActiveRecord::Base.establish_connection
+  # the following is *required* for Rails + "preload_app true",
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection(
+      :adapter => 'postgresql',
+      :encoding => 'unicode',
+      :pool => 5,
+      :database => ENV['STRINGER_DATABASE'] || "stringer",
+      :username => ENV['STRINGER_DATABASE_USERNAME'],
+      :password => ENV['STRINGER_DATABASE_PASSWORD']
+    )
 end
