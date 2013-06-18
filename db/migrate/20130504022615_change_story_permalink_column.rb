5752208758890861cc8fa1fea8895fada9d1e333
Matt Swanson <mdswanson@sep.com>
2013-06-17 20:23:16 -0400

    Add down migration for change_column type

--- db/migrate/20130504022615_change_story_permalink_column.rb
+++ db/migrate/20130504022615_change_story_permalink_column.rb
@@ -2,4 +2,8 @@ class ChangeStoryPermalinkColumn < ActiveRecord::Migration
   def up
     change_column :stories, :permalink, :text
   end
+
+  def down
+    change_column :stories, :permalink, :string
+  end
 end
