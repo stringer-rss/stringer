ff36b9681b06a93ad6cd8a28232966735c4f429b
Jeroen Jacobs <git@jeroenj.be>
2013-08-02 07:04:15 +0200

    Adds more specs related to finding new stories

--- spec/commands/find_new_stories_spec.rb
+++ spec/commands/find_new_stories_spec.rb
@@ -53,6 +53,26 @@ describe FindNewStories do
         result = FindNewStories.new(feed, Time.new(2013, 1, 3), old_story).new_stories
         result.should eq [new_story]
       end
+
+      it "should prefer scanning until the last entry_id over the last url" do
+        new_story = stub(published: nil, entry_id: "new-story", url: "http://blog.com/new-story")
+        duplicate_id_story = stub(published: nil, entry_id: "duplicate-id-story-story", url: "http://blog.com/new-story")
+        old_story = stub(published: nil, entry_id: "old-story", url: "http://blog.com/old-story")
+        feed = stub(last_modified: nil, entries: [new_story, duplicate_id_story, old_story])
+
+        result = FindNewStories.new(feed, Time.new(2013, 1, 3), duplicate_id_story).new_stories
+        result.should eq [new_story]
+      end
+
+      it "should not import new stories if they have the same url as a previous one when they have no entry_id" do
+        new_story = stub(published: nil, entry_id: nil, url: "http://blog.com/new-story")
+        duplicate_id_story = stub(published: nil, entry_id: nil, url: "http://blog.com/new-story")
+        old_story = stub(published: nil, entry_id: nil, url: "http://blog.com/old-story")
+        feed = stub(last_modified: nil, entries: [new_story, duplicate_id_story, old_story])
+
+        result = FindNewStories.new(feed, Time.new(2013, 1, 3), duplicate_id_story).new_stories
+        result.should eq []
+      end
     end
   end
 end
