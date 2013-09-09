44e0caffb68faa1385fbc763761e0cba8773409d
Victor Koronen <koronen@kth.se>
2013-09-09 20:46:20 +0200

    Split RemoveOldStories spec in two
    One assertion per test.

--- spec/tasks/remove_old_stories_spec.rb
+++ spec/tasks/remove_old_stories_spec.rb
@@ -2,18 +2,21 @@ require 'spec_helper'
 app_require 'tasks/remove_old_stories'
 
 describe RemoveOldStories do
-  before do
-    @arel_mock = double('arel')
-    @arel_mock.stub(:delete_all) { 0 }
-    StoryRepository.stub(:unstarred_read_stories_older_than) { @arel_mock }
-  end
-
   describe '.remove!' do
+    let(:stories_mock) { double('stories') }
+
     it 'should pass along the number of days to the story repository query' do
-      StoryRepository.should_receive(:unstarred_read_stories_older_than).with(7)
-      @arel_mock.should_receive(:delete_all)
+      StoryRepository.should_receive(:unstarred_read_stories_older_than).with(7).and_return(stories_mock)
+      stories_mock.stub(:delete_all)
 
       RemoveOldStories.remove!(7)
+    end
+
+    it 'should request deletion of all old stories' do
+      StoryRepository.should_receive(:unstarred_read_stories_older_than).and_return(stories_mock)
+      stories_mock.should_receive(:delete_all)
+
+      RemoveOldStories.remove!(11)
     end
   end
 end
