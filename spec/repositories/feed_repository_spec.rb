66398fd058eac16a589e39245bea733caf8e1269
Victor Koronen <koronen@kth.se>
2013-09-23 16:01:11 +0200

    Remove trailing whitespace

--- spec/repositories/feed_repository_spec.rb
+++ spec/repositories/feed_repository_spec.rb
@@ -9,17 +9,17 @@ describe FeedRepository do
 
     it "saves the last_fetched timestamp" do
       feed = Feed.new
-      
+
       result = FeedRepository.update_last_fetched(feed, timestamp)
 
       feed.last_fetched.should eq timestamp
     end
 
     let(:weird_timestamp) { Time.parse("Mon, 01 Jan 0001 00:00:00 +0100") }
-    
+
     it "rejects weird timestamps" do
       feed = Feed.new(last_fetched: timestamp)
-      
+
       result = FeedRepository.update_last_fetched(feed, weird_timestamp)
 
       feed.last_fetched.should eq timestamp
@@ -29,7 +29,7 @@ describe FeedRepository do
       feed = Feed.new(last_fetched: timestamp)
 
       result = FeedRepository.update_last_fetched(feed, nil)
-      
+
       feed.last_fetched.should eq timestamp
     end
   end
