bef091b943dcfc3e47c338868c5d9cb97a4f3cf6
Richard Z.H. Wang <zhwang@live.com.au>
2013-09-29 11:32:28 +1000

    Make tests for Google Reader stars import order independent

--- spec/commands/stories/import_from_google_reader_stars_spec.rb
+++ spec/commands/stories/import_from_google_reader_stars_spec.rb
@@ -12,29 +12,39 @@ describe ImportFromGoogleReaderStars do
     let(:feed_name) { "Ars Technica" }
     let(:feed_url) { "http://feeds.arstechnica.com/arstechnica/everything" }
 
-    before do
+    def create_feed_and_import_stars
       Feed.create(name: feed_name,
                   url: feed_url,
                   last_fetched: 0)
+
+      ImportFromGoogleReaderStars.import(starred_text)
+    end
+
+    def destroy_feed
+      Feed.where(name: feed_name).first.destroy
     end
 
     it "imports starred items" do
-      ImportFromGoogleReaderStars.import(starred_text)
+      create_feed_and_import_stars
 
       StoryRepository.all_starred.count.should eq 2
-      
+
       story1 = Story.where(title: story1_title)
       story1.count.should eq 1
       story1.first.body.should include story1_excerpt
       story1.first.is_starred.should eq true
 
-      story2 = Story.where(title: "Economists demonstrate exactly why bank robbery is a bad idea")
+      story2 = Story.where(title: story2_title)
       story2.count.should eq 1
       story2.first.body.should include story2_excerpt
       story2.first.is_starred.should eq true
+
+      destroy_feed
     end
 
     it "stars unstarred items" do
+      create_feed_and_import_stars
+
       story1 = Story.where(title: story1_title)
       story1.first.is_starred = false
       story1.first.entry_id = "the importer should rely on my permalink not my entry_id to decide i should be starred"
@@ -48,10 +58,13 @@ describe ImportFromGoogleReaderStars do
 
       story1 = Story.where(title: story1_title)
       story1.first.is_starred.should eq true
+
+      destroy_feed
     end
 
     it "skips unsubscribed feeds" do
-      Feed.where(name: feed_name).first.destroy
+      create_feed_and_import_stars
+      destroy_feed
 
       skipped_feeds = ImportFromGoogleReaderStars.import(starred_text)
 
