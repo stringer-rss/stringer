04133bc5569bb465fd7ba335cc6a0a316c2060b8
Bodo Tasche <bodo@wannawork.de>
2015-04-08 20:45:51 +0200

    Do not fix the title if the title is nil, fixes #373

--- db/migrate/20141102103617_fix_invalid_titles_with_unicode_line_endings.rb
+++ db/migrate/20141102103617_fix_invalid_titles_with_unicode_line_endings.rb
@@ -1,8 +1,10 @@
 class FixInvalidTitlesWithUnicodeLineEndings < ActiveRecord::Migration
   def up
     Story.find_each do |story|
-      valid_title = story.title.gsub("\u2028", '').gsub("\u2029", '')
-      story.update_attribute(:title, valid_title)
+      unless story.title.nil?
+        valid_title = story.title.gsub("\u2028", '').gsub("\u2029", '')
+        story.update_attribute(:title, valid_title)
+      end
     end
   end
 
