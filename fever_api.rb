dc939213417dfb07004812a3743a0c0888cfd725
Victor Koronen <koronen@kth.se>
2015-12-19 14:35:35 +0100

    Fix Style/ClassAndModuleChildren

--- fever_api.rb
+++ fever_api.rb
@@ -3,36 +3,38 @@ require "sinatra/activerecord"
 
 require_relative "./app/fever_api/response"
 
-class FeverAPI::Endpoint < Sinatra::Base
-  configure do
-    set :database_file, "config/database.yml"
+module FeverAPI
+  class Endpoint < Sinatra::Base
+    configure do
+      set :database_file, "config/database.yml"
 
-    register Sinatra::ActiveRecordExtension
-    ActiveRecord::Base.include_root_in_json = false
-  end
-
-  before do
-    halt 403 unless authenticated?(params[:api_key])
-  end
-
-  def authenticated?(api_key)
-    if api_key
-      user = User.first
-      user.api_key && api_key.downcase == user.api_key.downcase
+      register Sinatra::ActiveRecordExtension
+      ActiveRecord::Base.include_root_in_json = false
     end
-  end
 
-  get "/" do
-    content_type :json
-    build_response(params)
-  end
+    before do
+      halt 403 unless authenticated?(params[:api_key])
+    end
 
-  post "/" do
-    content_type :json
-    build_response(params)
-  end
+    def authenticated?(api_key)
+      if api_key
+        user = User.first
+        user.api_key && api_key.downcase == user.api_key.downcase
+      end
+    end
 
-  def build_response(params)
-    FeverAPI::Response.new(params).to_json
+    get "/" do
+      content_type :json
+      build_response(params)
+    end
+
+    post "/" do
+      content_type :json
+      build_response(params)
+    end
+
+    def build_response(params)
+      FeverAPI::Response.new(params).to_json
+    end
   end
 end
