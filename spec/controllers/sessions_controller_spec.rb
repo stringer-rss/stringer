01a048e9fb127739b203b9edde349ed80796c71a
Victor Koronen <koronen@kth.se>
2015-12-19 14:26:27 +0100

    Fix Style/AlignParameters

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -38,7 +38,7 @@ describe "SessionsController" do
       SignInUser.stub(:sign_in).and_return(double(id: 1))
 
       post "/login", { password: "the-password" },
-        'rack.session' => { redirect_to: '/archive' }
+           'rack.session' => { redirect_to: '/archive' }
 
       session[:redirect_to].should be_nil
       URI::parse(last_response.location).path.should eq "/archive"
