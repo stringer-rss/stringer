bb8da1c1aa786a0503d7c23517a1f25c50e8b772
Everton Ribeiro <nuxlli@gmail.com>
2016-03-10 01:04:43 -0300

    Passing only feed id instead of the whole object for Delayed Job

--- app/tasks/fetch_feeds.rb
+++ app/tasks/fetch_feeds.rb
@@ -3,12 +3,19 @@ require "thread/pool"
 require_relative "fetch_feed"
 
 class FetchFeeds
-  def initialize(feeds, pool = Thread.pool(10))
+  def initialize(feeds, pool = nil)
+    @pool  = pool
     @feeds = feeds
-    @pool = pool
+    @feeds_ids = []
   end
 
   def fetch_all
+    @pool ||= Thread.pool(10)
+
+    if @feeds.blank? and !@feeds_ids.blank?
+      @feeds = FeedRepository.fetch_by_ids(@feeds_ids)
+    end
+
     @feeds.each do |feed|
       @pool.process do
         FetchFeed.new(feed).fetch
@@ -20,7 +27,13 @@ class FetchFeeds
     @pool.shutdown
   end
 
+  def prepare_to_delay
+    @feeds_ids = @feeds.map { |feed| feed.id }
+    @feeds = []
+    return self
+  end
+
   def self.enqueue(feeds)
-    new(feeds).delay.fetch_all
+    new(feeds).prepare_to_delay.delay.fetch_all
   end
 end
