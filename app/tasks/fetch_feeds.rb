57a00ae09e2e02a46085f5e6800867358105a3dd
Everton Ribeiro <nuxlli@gmail.com>
2016-03-10 01:04:43 -0300

    Passing only feed id instead of the whole object for Delayed Job
    While running Stringer in a Docker container, there was an issue on passing the
    Feed data to the Delayed Job.
    
    The whole object was serialized by the application (FetchFeeds class) and
    deserialized by the Delayed Job.
    
    That's not a good practice and can cause several issues, as you can see in
    collectiveidea/delayed_job_active_record#101
    
    To solve this, instead of passing the whole Feed object to the Delayed Job, only
    its id attribute is sent. Then, the Feed is get from the database and its
    content is fetched normally.
    
    Plus, the @pool object (a pool of threads) was also serialized and sent, causing
    issues.
    If no pool is passed to the job, it should be instantiated from inside the job
    itself, avoiding the serialize/unserialize issues.
    Keeping the pool argument in the FetchFeeds#initialize method ensures the tests
    can still mock it and run the defined expect clauses.

--- app/tasks/fetch_feeds.rb
+++ app/tasks/fetch_feeds.rb
@@ -3,12 +3,17 @@ require "thread/pool"
 require_relative "fetch_feed"
 
 class FetchFeeds
-  def initialize(feeds, pool = Thread.pool(10))
+  def initialize(feeds, pool = nil)
+    @pool  = pool
     @feeds = feeds
-    @pool = pool
+    @feeds_ids = []
   end
 
   def fetch_all
+    @pool ||= Thread.pool(10)
+
+    @feeds = FeedRepository.fetch_by_ids(@feeds_ids) if @feeds.blank? && !@feeds_ids.blank?
+
     @feeds.each do |feed|
       @pool.process do
         FetchFeed.new(feed).fetch
@@ -20,7 +25,13 @@ class FetchFeeds
     @pool.shutdown
   end
 
+  def prepare_to_delay
+    @feeds_ids = @feeds.map(&:id)
+    @feeds = []
+    self
+  end
+
   def self.enqueue(feeds)
-    new(feeds).delay.fetch_all
+    new(feeds).prepare_to_delay.delay.fetch_all
   end
 end
