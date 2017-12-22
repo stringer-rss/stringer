18486be092c5334bbd3e4b26074398610d38652c
Michael Stock <mikeastock@gmail.com>
2017-12-21 19:24:10 -0800

    Move to using Feedjira 3.0
    Feedjira removed it's built-in HTTP fetching so that has been replaced
    with HTTParty.

--- app/tasks/fetch_feed.rb
+++ app/tasks/fetch_feed.rb
@@ -1,13 +1,15 @@
 require "feedjira"
+require "httparty"
 
 require_relative "../repositories/story_repository"
 require_relative "../repositories/feed_repository"
 require_relative "../commands/feeds/find_new_stories"
 
 class FetchFeed
-  def initialize(feed, parser: Feedjira::Feed, logger: nil)
+  def initialize(feed, parser: Feedjira, client: HTTParty, logger: nil)
     @feed = feed
     @parser = parser
+    @client = client
     @logger = logger
   end
 
@@ -30,7 +32,8 @@ class FetchFeed
   private
 
   def fetch_raw_feed
-    @parser.fetch_and_parse(@feed.url)
+    response = @client.get(@feed.url)
+    @parser.parse(response)
   end
 
   def feed_not_modified
