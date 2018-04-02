7cc2d25649987cdbad3e9f728e2c6fe622ce435d
Gabriel Poça <gabrielpoca@gmail.com>
2018-04-02 12:14:27 +0100

    use worker

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
-console: bundle exec racksh
+worker: bundle exec rake work_jobs
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,16 +2,10 @@ worker_processes 1
 timeout 30
 preload_app true
 
-@delayed_job_pid = nil
-
 before_fork do |_server, _worker|
   # the following is highly recommended for Rails + "preload_app true"
   # as there's no need for the master process to hold a connection
   ActiveRecord::Base.connection.disconnect! if defined?(ActiveRecord::Base)
-
-  @delayed_job_pid ||= spawn("bundle exec rake work_jobs") unless ENV["WORKER_EMBEDDED"] == "false"
-
-  sleep 1
 end
 
 after_fork do |_server, _worker|
