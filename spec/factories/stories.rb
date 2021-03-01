c9da11f3ad2de48145919b18e59a009861555a35
Robert Fletcher <mockdeep@users.noreply.github.com>
2021-03-01 10:46:47 -0800

    Specs: add more tests for StoryRepository (#552)

--- spec/factories/stories.rb
+++ spec/factories/stories.rb
@@ -1,9 +1,15 @@
 module Factories
-  def create_story(params = {})
-    build_story(params).tap(&:save!)
+  STORY_TRAITS = {
+    unread: -> { { is_read: false } }
+  }.freeze
+
+  def create_story(*traits, **params)
+    build_story(*traits, **params).tap(&:save!)
   end
 
-  def build_story(params = {})
+  def build_story(*traits, **params)
+    traits.each { |trait| params.merge!(STORY_TRAITS.fetch(trait).call) }
+
     Story.new(entry_id: next_id, feed: build_feed, **params)
   end
 end
