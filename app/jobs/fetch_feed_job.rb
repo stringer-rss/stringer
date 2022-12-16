6799fe0428a1a6d2ce62d0eff0d3474e1478d3d1
Robert Fletcher <lobatifricha@gmail.com>
2022-12-15 20:43:39 -0800

    RuboCop: enable Layout/MultilineAssignmentLayout (#682)

--- app/jobs/fetch_feed_job.rb
+++ app/jobs/fetch_feed_job.rb
@@ -1,6 +1,7 @@
-FetchFeedJob = Struct.new(:feed_id) do
-  def perform
-    feed = FeedRepository.fetch(feed_id)
-    FetchFeed.new(feed).fetch
+FetchFeedJob =
+  Struct.new(:feed_id) do
+    def perform
+      feed = FeedRepository.fetch(feed_id)
+      FetchFeed.new(feed).fetch
+    end
   end
-end
