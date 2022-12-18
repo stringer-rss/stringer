c75d69a6bd2aad15a1c4da3f633c2ab53dbe6206
Robert Fletcher <lobatifricha@gmail.com>
2022-12-18 10:59:41 -0800

    Rails: move feed new/create/destroy to ActionController

--- app/controllers/sinatra/feeds_controller.rb
+++ app/controllers/sinatra/feeds_controller.rb
@@ -3,35 +3,6 @@ require_relative "../../commands/feeds/add_new_feed"
 require_relative "../../commands/feeds/export_to_opml"
 
 class Stringer < Sinatra::Base
-  delete "/feeds/:feed_id" do
-    FeedRepository.delete(params[:feed_id])
-
-    status 200
-  end
-
-  get "/feeds/new" do
-    @feed_url = params[:feed_url]
-    erb :'feeds/add'
-  end
-
-  post "/feeds" do
-    @feed_url = params[:feed_url]
-    feed = AddNewFeed.add(@feed_url)
-
-    if feed && feed.valid?
-      FetchFeeds.enqueue([feed])
-
-      flash[:success] = t("feeds.add.flash.added_successfully")
-      redirect to("/")
-    elsif feed
-      flash.now[:error] = t("feeds.add.flash.already_subscribed_error")
-      erb :'feeds/add'
-    else
-      flash.now[:error] = t("feeds.add.flash.feed_not_found_error")
-      erb :'feeds/add'
-    end
-  end
-
   get "/feeds/import" do
     erb :'feeds/import'
   end
