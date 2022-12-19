b6b4077c309ec6417a84003160ca9f73dd3f2182
Robert Fletcher <lobatifricha@gmail.com>
2022-12-18 22:54:50 -0800

    RuboCop: fix RedundantLineBreak offenses

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -4,9 +4,6 @@ class UserFactory
   class FakeUser < OpenStruct; end
 
   def self.build
-    FakeUser.new(
-      id: rand(100),
-      setup_complete: false
-    )
+    FakeUser.new(id: rand(100), setup_complete: false)
   end
 end
