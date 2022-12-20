b4b976848f21cee44653f68a26eff09b01149f0b
Robert Fletcher <lobatifricha@gmail.com>
2022-12-20 11:55:07 -0800

    Factories: switch groups to FactoryBot

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,9 +1,3 @@
-module Factories
-  def create_group(params = {})
-    build_group(params).tap(&:save!)
-  end
-
-  def build_group(params = {})
-    Group.new(**params)
-  end
+FactoryBot.define do
+  factory(:group)
 end
