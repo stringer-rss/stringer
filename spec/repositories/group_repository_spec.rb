7681b79a37f47b5873fece07a80f47cd1a1e993e
Robert Fletcher <lobatifricha@gmail.com>
2022-12-20 16:23:50 -0800

    Factories: switch groups to FactoryBot (#736)

--- spec/repositories/group_repository_spec.rb
+++ spec/repositories/group_repository_spec.rb
@@ -6,10 +6,10 @@ app_require "repositories/group_repository"
 describe GroupRepository do
   describe ".list" do
     it "lists groups ordered by lower name" do
-      group1 = create_group(name: "Zabba")
-      group2 = create_group(name: "zlabba")
-      group3 = create_group(name: "blabba")
-      group4 = create_group(name: "Babba")
+      group1 = create(:group, name: "Zabba")
+      group2 = create(:group, name: "zlabba")
+      group3 = create(:group, name: "blabba")
+      group4 = create(:group, name: "Babba")
       expected_groups = [group4, group3, group1, group2]
 
       expect(GroupRepository.list).to eq(expected_groups)
