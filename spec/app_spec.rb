2e18794c5dc102204146d10fcf54f026e283ef2f
Robert Fletcher <lobatifricha@gmail.com>
2023-01-21 19:03:47 -0800

    Rails: move root path to ActionController

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -11,48 +11,4 @@ describe "App" do
       expect(Rails.application.config.cache_classes).to be(true)
     end
   end
-
-  context "when user is not authenticated and page requires authentication" do
-    it "sets the session redirect_to" do
-      create(:user)
-
-      get("/news")
-
-      expect(session[:redirect_to]).to eq("/news")
-    end
-
-    it "redirects to /login" do
-      create(:user)
-
-      get("/news")
-
-      expect(last_response).to be_redirect
-      expect(last_response.headers["Location"]).to end_with("/login")
-    end
-  end
-
-  it "does not redirect when page needs no authentication" do
-    create(:user)
-
-    get("/login")
-
-    expect(last_response).not_to be_redirect
-  end
-
-  it "does not redirect when user is authenticated" do
-    login_as(create(:user))
-
-    get("/news")
-
-    expect(last_response).not_to be_redirect
-  end
-
-  it "redirects '/' to '/news'" do
-    login_as(create(:user))
-
-    get("/")
-
-    expect(last_response).to be_redirect
-    expect(last_response.headers["Location"]).to end_with("/news")
-  end
 end
