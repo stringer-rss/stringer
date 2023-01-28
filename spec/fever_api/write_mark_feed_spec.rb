4e680d1faf6c61187f7119e838f6f62e9efa1146
Robert Fletcher <lobatifricha@gmail.com>
2023-01-27 16:10:21 -0800

    RuboCop: fix NumericLiteral violations (#842)

--- spec/fever_api/write_mark_feed_spec.rb
+++ spec/fever_api/write_mark_feed_spec.rb
@@ -12,9 +12,9 @@ describe FeverAPI::WriteMarkFeed do
 
   it "instantiates a feed marker and calls mark_feed_as_read if requested" do
     expect(marker_class)
-      .to receive(:new).with(5, 1234567890).and_return(feed_marker)
+      .to receive(:new).with(5, 1_234_567_890).and_return(feed_marker)
     expect(feed_marker).to receive(:mark_feed_as_read)
-    expect(subject.call(mark: "feed", id: 5, before: 1234567890)).to eq({})
+    expect(subject.call(mark: "feed", id: 5, before: 1_234_567_890)).to eq({})
   end
 
   it "returns an empty hash otherwise" do
