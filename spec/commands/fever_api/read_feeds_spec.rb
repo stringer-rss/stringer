4800cd3bdbffdce37c8503e27ec624334ba08029
Robert Fletcher <lobatifricha@gmail.com>
2023-02-13 21:40:06 -0800

    Callable: make ReadFeeds callable

--- spec/commands/fever_api/read_feeds_spec.rb
+++ spec/commands/fever_api/read_feeds_spec.rb
@@ -3,26 +3,14 @@
 require "spec_helper"
 
 describe FeverAPI::ReadFeeds do
-  subject { described_class.new(feed_repository:) }
-
-  let(:feed_ids) { [5, 7, 11] }
-  let(:feeds) do
-    feed_ids.map { |id| double("feed", id:, as_fever_json: { id: }) }
-  end
-  let(:feed_repository) { double("repo") }
-
   it "returns a list of feeds" do
-    expect(feed_repository).to receive(:list).and_return(feeds)
-    expect(subject.call("feeds" => nil)).to eq(
-      feeds: [
-        { id: 5 },
-        { id: 7 },
-        { id: 11 }
-      ]
+    feeds = create_list(:feed, 3)
+    expect(described_class.call("feeds" => nil)).to eq(
+      feeds: feeds.map(&:as_fever_json)
     )
   end
 
   it "returns an empty hash otherwise" do
-    expect(subject.call).to eq({})
+    expect(described_class.call({})).to eq({})
   end
 end
