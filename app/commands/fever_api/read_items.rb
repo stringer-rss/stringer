52424a631bf1773be57c4831186d15e09119de63
Robert Fletcher <lobatifricha@gmail.com>
2023-02-13 21:56:50 -0800

    Callable: make ReadItems callable

--- app/commands/fever_api/read_items.rb
+++ app/commands/fever_api/read_items.rb
@@ -1,54 +1,48 @@
 # frozen_string_literal: true
 
 module FeverAPI
-  class ReadItems
-    def self.call(params)
-      new.call(params)
-    end
+  module ReadItems
+    class << self
+      def call(params)
+        if params.keys.include?("items")
+          item_ids =
+            begin
+              params[:with_ids].split(",")
+            rescue StandardError
+              nil
+            end
 
-    def initialize(options = {})
-      @story_repository = options.fetch(:story_repository) { StoryRepository }
-    end
-
-    def call(params = {})
-      if params.keys.include?("items")
-        item_ids =
-          begin
-            params[:with_ids].split(",")
-          rescue StandardError
-            nil
-          end
-
-        {
-          items: items(item_ids, params[:since_id]),
-          total_items: total_items(item_ids)
-        }
-      else
-        {}
+          {
+            items: items(item_ids, params[:since_id]),
+            total_items: total_items(item_ids)
+          }
+        else
+          {}
+        end
       end
-    end
 
-    private
+      private
 
-    def items(item_ids, since_id)
-      items = item_ids ? stories_by_ids(item_ids) : unread_stories(since_id)
-      items.map(&:as_fever_json)
-    end
+      def items(item_ids, since_id)
+        items = item_ids ? stories_by_ids(item_ids) : unread_stories(since_id)
+        items.map(&:as_fever_json)
+      end
 
-    def total_items(item_ids)
-      items = item_ids ? stories_by_ids(item_ids) : unread_stories
-      items.count
-    end
+      def total_items(item_ids)
+        items = item_ids ? stories_by_ids(item_ids) : unread_stories
+        items.count
+      end
 
-    def stories_by_ids(ids)
-      @story_repository.fetch_by_ids(ids)
-    end
+      def stories_by_ids(ids)
+        StoryRepository.fetch_by_ids(ids)
+      end
 
-    def unread_stories(since_id = nil)
-      if since_id
-        @story_repository.unread_since_id(since_id)
-      else
-        @story_repository.unread
+      def unread_stories(since_id = nil)
+        if since_id
+          StoryRepository.unread_since_id(since_id)
+        else
+          StoryRepository.unread
+        end
       end
     end
   end
--- spec/commands/fever_api/read_items_spec.rb
+++ spec/commands/fever_api/read_items_spec.rb
@@ -3,67 +3,35 @@
 require "spec_helper"
 
 describe FeverAPI::ReadItems do
-  subject { described_class.new(story_repository:) }
-
-  let(:story_repository) { double("repo") }
-
   it "returns a list of unread items including total count" do
-    stories = [
-      double("story", as_fever_json: { id: 5 }),
-      double("story", as_fever_json: { id: 7 }),
-      double("story", as_fever_json: { id: 11 })
-    ]
-    expect(story_repository).to receive(:unread).twice.and_return(stories)
-    expect(subject.call("items" => nil)).to eq(
-      items: [
-        { id: 5 },
-        { id: 7 },
-        { id: 11 }
-      ],
+    stories = create_list(:story, 3, :unread)
+
+    expect(described_class.call("items" => nil)).to eq(
+      items: stories.map(&:as_fever_json),
       total_items: 3
     )
   end
 
   it "returns a list of unread items since id including total count" do
-    unread_since_stories = [
-      double("story", as_fever_json: { id: 5 }),
-      double("story", as_fever_json: { id: 7 })
-    ]
-    expect(story_repository)
-      .to receive(:unread_since_id).with(3).and_return(unread_since_stories)
-    unread_stories = [
-      double("story", as_fever_json: { id: 2 }),
-      double("story", as_fever_json: { id: 5 }),
-      double("story", as_fever_json: { id: 7 })
-    ]
-    expect(story_repository).to receive(:unread).and_return(unread_stories)
-    expect(subject.call("items" => nil, since_id: 3)).to eq(
-      items: [
-        { id: 5 },
-        { id: 7 }
-      ],
+    story1, *other_stories = create_list(:story, 3, :unread)
+
+    expect(described_class.call("items" => nil, since_id: story1.id)).to eq(
+      items: other_stories.map(&:as_fever_json),
       total_items: 3
     )
   end
 
   it "returns a list of specified items including total count" do
-    stories = [
-      double("story", as_fever_json: { id: 5 }),
-      double("story", as_fever_json: { id: 11 })
-    ]
-    expect(story_repository)
-      .to receive(:fetch_by_ids).with(["5", "11"]).twice.and_return(stories)
+    _story1, *other_stories = create_list(:story, 3)
+    with_ids = other_stories.map(&:id)
 
-    expect(subject.call("items" => nil, with_ids: "5,11")).to eq(
-      items: [
-        { id: 5 },
-        { id: 11 }
-      ],
+    expect(described_class.call("items" => nil, with_ids:)).to eq(
+      items: other_stories.map(&:as_fever_json),
       total_items: 2
     )
   end
 
   it "returns an empty hash otherwise" do
-    expect(subject.call).to eq({})
+    expect(described_class.call({})).to eq({})
   end
 end
