b149a57cf371570edd802c7042742bc97b77ec3b
Robert Fletcher <lobatifricha@gmail.com>
2023-02-13 21:48:09 -0800

    Callable: make ReadGroups callable

--- spec/commands/fever_api/read_groups_spec.rb
+++ spec/commands/fever_api/read_groups_spec.rb
@@ -3,31 +3,15 @@
 require "spec_helper"
 
 describe FeverAPI::ReadGroups do
-  subject { described_class.new(group_repository:) }
-
-  let(:group1) { double("group1", as_fever_json: { id: 1, title: "IT news" }) }
-  let(:group2) do
-    double("group2", as_fever_json: { id: 2, title: "World news" })
-  end
-  let(:group_repository) { double("repo") }
-
   it "returns a group list if requested" do
-    expect(group_repository).to receive(:list).and_return([group1, group2])
-    expect(subject.call("groups" => nil)).to eq(
-      groups: [
-        {
-          id: 1,
-          title: "IT news"
-        },
-        {
-          id: 2,
-          title: "World news"
-        }
-      ]
+    groups = create_pair(:group)
+
+    expect(described_class.call("groups" => nil)).to eq(
+      groups: groups.map(&:as_fever_json)
     )
   end
 
   it "returns an empty hash otherwise" do
-    expect(subject.call).to eq({})
+    expect(described_class.call({})).to eq({})
   end
 end
