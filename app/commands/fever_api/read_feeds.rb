11ef7343728053ac69999449d60a32eaac798cde
Robert Fletcher <lobatifricha@gmail.com>
2023-02-13 21:40:06 -0800

    Callable: make ReadFeeds callable

--- app/commands/fever_api/read_feeds.rb
+++ app/commands/fever_api/read_feeds.rb
@@ -1,27 +1,21 @@
 # frozen_string_literal: true
 
 module FeverAPI
-  class ReadFeeds
-    def self.call(params)
-      new.call(params)
-    end
-
-    def initialize(options = {})
-      @feed_repository = options.fetch(:feed_repository) { FeedRepository }
-    end
-
-    def call(params = {})
-      if params.keys.include?("feeds")
-        { feeds: }
-      else
-        {}
+  module ReadFeeds
+    class << self
+      def call(params)
+        if params.keys.include?("feeds")
+          { feeds: }
+        else
+          {}
+        end
       end
-    end
 
-    private
+      private
 
-    def feeds
-      @feed_repository.list.map(&:as_fever_json)
+      def feeds
+        FeedRepository.list.map(&:as_fever_json)
+      end
     end
   end
 end
