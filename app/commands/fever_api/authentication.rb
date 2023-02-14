57887db2d97d2e45cfdf512c663308880d857611
Robert Fletcher <lobatifricha@gmail.com>
2023-02-13 22:09:57 -0800

    Callable: make Authentication callable (#884)

--- app/commands/fever_api/authentication.rb
+++ app/commands/fever_api/authentication.rb
@@ -1,16 +1,8 @@
 # frozen_string_literal: true
 
 module FeverAPI
-  class Authentication
-    def self.call(params)
-      new.call(params)
-    end
-
-    def initialize(options = {})
-      @clock = options.fetch(:clock) { Time }
-    end
-
-    def call(_params)
+  module Authentication
+    def self.call(_params)
       last_refreshed_on_time = (Feed.maximum(:last_fetched) || 0).to_i
 
       { auth: 1, last_refreshed_on_time: }
