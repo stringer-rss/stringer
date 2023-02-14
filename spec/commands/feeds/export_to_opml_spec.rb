aec62cbc7a535c51a28671c22b9720114160f83e
Robert Fletcher <lobatifricha@gmail.com>
2023-02-13 17:06:06 -0800

    Callable: make ExportToOpml callable (#880)

--- spec/commands/feeds/export_to_opml_spec.rb
+++ spec/commands/feeds/export_to_opml_spec.rb
@@ -3,34 +3,32 @@
 require "spec_helper"
 
 describe ExportToOpml do
-  describe "#to_xml" do
-    let(:feed_one) { build(:feed)      }
-    let(:feed_two) { build(:feed)      }
-    let(:feeds) { [feed_one, feed_two] }
+  let(:feed_one) { build(:feed)      }
+  let(:feed_two) { build(:feed)      }
+  let(:feeds) { [feed_one, feed_two] }
 
-    it "returns OPML XML" do
-      result = described_class.new(feeds).to_xml
+  it "returns OPML XML" do
+    result = described_class.call(feeds)
 
-      outlines = Nokogiri.XML(result).xpath("//body//outline")
-      expect(outlines.size).to eq(2)
-      expect(outlines.first["title"]).to eq(feed_one.name)
-      expect(outlines.first["xmlUrl"]).to eq(feed_one.url)
-      expect(outlines.last["title"]).to eq(feed_two.name)
-      expect(outlines.last["xmlUrl"]).to eq(feed_two.url)
-    end
+    outlines = Nokogiri.XML(result).xpath("//body//outline")
+    expect(outlines.size).to eq(2)
+    expect(outlines.first["title"]).to eq(feed_one.name)
+    expect(outlines.first["xmlUrl"]).to eq(feed_one.url)
+    expect(outlines.last["title"]).to eq(feed_two.name)
+    expect(outlines.last["xmlUrl"]).to eq(feed_two.url)
+  end
 
-    it "handles empty feeds" do
-      result = described_class.new([]).to_xml
+  it "handles empty feeds" do
+    result = described_class.call([])
 
-      outlines = Nokogiri.XML(result).xpath("//body//outline")
-      expect(outlines).to be_empty
-    end
+    outlines = Nokogiri.XML(result).xpath("//body//outline")
+    expect(outlines).to be_empty
+  end
 
-    it "has a proper title" do
-      result = described_class.new(feeds).to_xml
+  it "has a proper title" do
+    result = described_class.call(feeds)
 
-      title = Nokogiri.XML(result).xpath("//head//title").first
-      expect(title.content).to eq("Feeds from Stringer")
-    end
+    title = Nokogiri.XML(result).xpath("//head//title").first
+    expect(title.content).to eq("Feeds from Stringer")
   end
 end
