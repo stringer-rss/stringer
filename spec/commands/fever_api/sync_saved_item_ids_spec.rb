b1e5e4c234051731d9f322485082d4a4f1077257
Robert Fletcher <lobatifricha@gmail.com>
2023-02-19 11:54:16 -0800

    Callable: make SyncSavedItemIds callable

--- spec/commands/fever_api/sync_saved_item_ids_spec.rb
+++ spec/commands/fever_api/sync_saved_item_ids_spec.rb
@@ -3,19 +3,13 @@
 require "spec_helper"
 
 describe FeverAPI::SyncSavedItemIds do
-  subject { described_class.new(story_repository:) }
-
-  let(:story_ids) { [5, 7, 11] }
-  let(:stories) { story_ids.map { |id| double("story", id:) } }
-  let(:story_repository) { double("repo") }
-
   it "returns a list of starred items if requested" do
-    expect(story_repository).to receive(:all_starred).and_return(stories)
-    expect(subject.call("saved_item_ids" => nil))
-      .to eq(saved_item_ids: story_ids.join(","))
+    stories = create_list(:story, 3, :starred)
+    expect(described_class.call("saved_item_ids" => nil))
+      .to eq(saved_item_ids: stories.map(&:id).join(","))
   end
 
   it "returns an empty hash otherwise" do
-    expect(subject.call).to eq({})
+    expect(described_class.call({})).to eq({})
   end
 end
