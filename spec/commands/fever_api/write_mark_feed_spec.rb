97e2a7df2104de5a34919f019da2d52e57b7a976
Robert Fletcher <lobatifricha@gmail.com>
2023-02-19 12:11:34 -0800

    Callable: make WriteMarkFeed a callable module

--- spec/commands/fever_api/write_mark_feed_spec.rb
+++ spec/commands/fever_api/write_mark_feed_spec.rb
@@ -1,19 +1,28 @@
 # frozen_string_literal: true
 
 RSpec.describe FeverAPI::WriteMarkFeed do
-  subject { described_class.new(marker_class:) }
-
   let(:feed_marker) { double("feed marker") }
   let(:marker_class) { double("marker class") }
 
-  it "instantiates a feed marker and calls mark_feed_as_read if requested" do
-    expect(marker_class)
-      .to receive(:new).with(5, 1_234_567_890).and_return(feed_marker)
-    expect(feed_marker).to receive(:mark_feed_as_read)
-    expect(subject.call(mark: "feed", id: 5, before: 1_234_567_890)).to eq({})
+  it "marks the feed stories as read before the given timestamp" do
+    feed = create(:feed)
+    story = create(:story, :unread, feed:, created_at: 1.week.ago)
+    before = 1.day.ago.to_i
+
+    expect { described_class.call(mark: "feed", id: feed.id, before:) }
+      .to change { story.reload.is_read? }.from(false).to(true)
+  end
+
+  it "does not mark the feed stories as read after the given timestamp" do
+    feed = create(:feed)
+    story = create(:story, :unread, feed:)
+    before = 1.day.ago.to_i
+
+    expect { described_class.call(mark: "feed", id: feed.id, before:) }
+      .to not_change { story.reload.is_read? }.from(false)
   end
 
   it "returns an empty hash otherwise" do
-    expect(subject.call).to eq({})
+    expect(described_class.call({})).to eq({})
   end
 end
