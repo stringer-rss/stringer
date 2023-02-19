4b644aa4e25fee57d26a67ee094e44238db2372a
Robert Fletcher <lobatifricha@gmail.com>
2023-02-19 11:54:23 -0800

    Callable: make ReadGroups callable (#892)

--- app/commands/fever_api/read_groups.rb
+++ app/commands/fever_api/read_groups.rb
@@ -1,27 +1,21 @@
 # frozen_string_literal: true
 
 module FeverAPI
-  class ReadGroups
-    def self.call(params)
-      new.call(params)
-    end
-
-    def initialize(options = {})
-      @group_repository = options.fetch(:group_repository) { GroupRepository }
-    end
-
-    def call(params = {})
-      if params.keys.include?("groups")
-        { groups: }
-      else
-        {}
+  module ReadGroups
+    class << self
+      def call(params)
+        if params.keys.include?("groups")
+          { groups: }
+        else
+          {}
+        end
       end
-    end
 
-    private
+      private
 
-    def groups
-      @group_repository.list.map(&:as_fever_json)
+      def groups
+        GroupRepository.list.map(&:as_fever_json)
+      end
     end
   end
 end
