9d79a31b4c0b696cc614aedba96e18e09eb49aad
Robert Fletcher <lobatifricha@gmail.com>
2023-02-21 21:25:03 -0800

    Capybara: add system test for feed imports (#918)

---
+++ spec/system/import_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+RSpec.describe "importing feeds", type: :system do
+  it "allows importing feeds" do
+    login_as(default_user)
+    visit(feeds_import_path)
+    file_path = Rails.root.join("spec/fixtures/feeds.opml")
+
+    attach_file("opml_file", file_path, visible: false)
+
+    expect(page).to have_content("We're getting you some stories to read")
+  end
+end
