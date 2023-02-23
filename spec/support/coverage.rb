caa64ffd0fa387ae22069f00d9a395fb02a104b3
Robert Fletcher <lobatifricha@gmail.com>
2023-02-22 20:29:47 -0800

    Coverage: add coverage for last branch

--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -17,4 +17,4 @@ SimpleCov.start(:rails) do
   add_group("Utils", "app/utils")
   enable_coverage :branch
 end
-SimpleCov.minimum_coverage(line: 100, branch: 98)
+SimpleCov.minimum_coverage(line: 100, branch: 100)
