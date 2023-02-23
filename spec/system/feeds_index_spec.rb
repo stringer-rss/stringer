d17ee7d9fadff5875d497bf256966540e599e4cd
Robert Fletcher <lobatifricha@gmail.com>
2023-02-22 20:48:39 -0800

    Capybara: add specs for feeds/index

---
+++ spec/system/feeds_index_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+RSpec.describe "feeds/index", type: :system do
+  it "displays a list of feeds" do
+    login_as(default_user)
+    create_pair(:feed)
+
+    visit "/feeds"
+
+    expect(page).to have_selector("li.feed", count: 2)
+  end
+
+  it "displays message to add feeds if there are none" do
+    login_as(default_user)
+
+    visit "/feeds"
+
+    expect(page).to have_content("Hey, you should add some feeds")
+  end
+
+  it "allows the user to delete a feed" do
+    login_as(default_user)
+    create(:feed)
+
+    visit "/feeds"
+    click_on "Delete"
+
+    expect(page).to have_content("Feed deleted")
+  end
+
+  it "allows the user to edit a feed" do
+    login_as(default_user)
+    feed = create(:feed)
+
+    visit "/feeds"
+    click_on "Edit"
+
+    expect(page).to have_field("Feed Name", with: feed.name)
+  end
+
+  it "links to the feed" do
+    login_as(default_user)
+    feed = create(:feed)
+
+    visit "/feeds"
+
+    expect(page).to have_link(href: feed.url)
+  end
+end
