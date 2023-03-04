59f481650d9fd86bb181bab080f581723877f47d
Robert Fletcher <lobatifricha@gmail.com>
2023-03-04 10:07:12 -0800

    Callable: make WriteMarkFeed a callable module (#952)

--- app/commands/fever_api/write_mark_feed.rb
+++ app/commands/fever_api/write_mark_feed.rb
@@ -1,18 +1,10 @@
 # frozen_string_literal: true
 
 module FeverAPI
-  class WriteMarkFeed
+  module WriteMarkFeed
     def self.call(params)
-      new.call(params)
-    end
-
-    def initialize(options = {})
-      @marker_class = options.fetch(:marker_class) { MarkFeedAsRead }
-    end
-
-    def call(params = {})
       if params[:mark] == "feed"
-        @marker_class.new(params[:id], params[:before]).mark_feed_as_read
+        MarkFeedAsRead.new(params[:id], params[:before]).mark_feed_as_read
       end
 
       {}
