4a87bc203ef98cf0744735fbc4740dd86f6bfeee
Robert Fletcher <lobatifricha@gmail.com>
2023-03-04 11:43:05 -0800

    Callable: remove parser argument from FetchFeed

--- spec/tasks/fetch_feed_spec.rb
+++ spec/tasks/fetch_feed_spec.rb
@@ -20,25 +20,20 @@ RSpec.describe FetchFeed do
     context "when feed has not been modified" do
       it "does not try to fetch posts" do
         client = class_spy(HTTParty)
-        parser = class_double(Feedjira, parse: 304)
+        expect(Feedjira).to receive(:parse).and_return(304)
 
         expect(StoryRepository).not_to receive(:add)
 
-        described_class.new(
-          daring_fireball,
-          parser:,
-          client:,
-          logger: nil
-        ).fetch
+        described_class.new(daring_fireball, client:, logger: nil).fetch
       end
 
       it "logs a message" do
         client = class_spy(HTTParty)
-        parser = class_double(Feedjira, parse: 304)
+        expect(Feedjira).to receive(:parse).and_return(304)
         output = StringIO.new
         logger = Logger.new(output)
 
-        described_class.new(daring_fireball, parser:, client:, logger:).fetch
+        described_class.new(daring_fireball, client:, logger:).fetch
 
         expect(output.string).to include("has not been modified")
       end
@@ -48,13 +43,13 @@ RSpec.describe FetchFeed do
       it "does not add any new posts" do
         fake_feed = double(last_modified: Time.zone.local(2012, 12, 31))
         client = class_spy(HTTParty)
-        parser = class_double(Feedjira, parse: fake_feed)
+        expect(Feedjira).to receive(:parse).and_return(fake_feed)
 
         allow(FindNewStories).to receive(:call).and_return([])
 
         expect(StoryRepository).not_to receive(:add)
 
-        described_class.new(daring_fireball, parser:, client:).fetch
+        described_class.new(daring_fireball, client:).fetch
       end
     end
 
@@ -67,13 +62,13 @@ RSpec.describe FetchFeed do
         double(last_modified: now, entries: [new_story, old_story])
       end
       let(:fake_client) { class_spy(HTTParty) }
-      let(:fake_parser) { class_double(Feedjira, parse: fake_feed) }
 
       before do
         allow(FindNewStories).to receive(:call).and_return([new_story])
       end
 
       it "only adds posts that are new" do
+        expect(Feedjira).to receive(:parse).and_return(fake_feed)
         expect(StoryRepository).to receive(:add).with(
           new_story,
           daring_fireball
@@ -81,22 +76,15 @@ RSpec.describe FetchFeed do
         expect(StoryRepository)
           .not_to receive(:add).with(old_story, daring_fireball)
 
-        described_class.new(
-          daring_fireball,
-          parser: fake_parser,
-          client: fake_client
-        ).fetch
+        described_class.new(daring_fireball, client: fake_client).fetch
       end
 
       it "updates the last fetched time for the feed" do
+        expect(Feedjira).to receive(:parse).and_return(fake_feed)
         expect(FeedRepository).to receive(:update_last_fetched)
           .with(daring_fireball, now)
 
-        described_class.new(
-          daring_fireball,
-          parser: fake_parser,
-          client: fake_client
-        ).fetch
+        described_class.new(daring_fireball, client: fake_client).fetch
       end
     end
 
@@ -105,36 +93,31 @@ RSpec.describe FetchFeed do
         fake_feed =
           double(last_modified: Time.zone.local(2012, 12, 31), entries: [])
         client = class_spy(HTTParty)
-        parser = class_double(Feedjira, parse: fake_feed)
+        expect(Feedjira).to receive(:parse).and_return(fake_feed)
 
         expect(FeedRepository).to receive(:set_status)
           .with(:green, daring_fireball)
 
-        described_class.new(daring_fireball, parser:, client:).fetch
+        described_class.new(daring_fireball, client:).fetch
       end
 
       it "sets the status to red if things go wrong" do
         client = class_spy(HTTParty)
-        parser = class_double(Feedjira, parse: 404)
+        expect(Feedjira).to receive(:parse).and_return(404)
 
         expect(FeedRepository).to receive(:set_status)
           .with(:red, daring_fireball)
 
-        described_class.new(
-          daring_fireball,
-          parser:,
-          client:,
-          logger: nil
-        ).fetch
+        described_class.new(daring_fireball, client:, logger: nil).fetch
       end
 
       it "outputs a message when things go wrong" do
         client = class_spy(HTTParty)
-        parser = class_double(Feedjira, parse: 404)
+        expect(Feedjira).to receive(:parse).and_return(404)
         output = StringIO.new
         logger = Logger.new(output)
 
-        described_class.new(daring_fireball, parser:, client:, logger:).fetch
+        described_class.new(daring_fireball, client:, logger:).fetch
 
         expect(output.string).to include("Something went wrong")
       end
