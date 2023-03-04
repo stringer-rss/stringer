0dba2437840cf634272acb246213f802333a4170
Robert Fletcher <lobatifricha@gmail.com>
2023-03-04 12:13:23 -0800

    Callable: make MarkFeedAsRead a callable module (#961)

--- app/commands/stories/mark_feed_as_read.rb
+++ app/commands/stories/mark_feed_as_read.rb
@@ -1,17 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "../../repositories/story_repository"
-
-class MarkFeedAsRead
-  def initialize(feed_id, timestamp, repository = StoryRepository)
-    @feed_id   = feed_id.to_i
-    @repo      = repository
-    @timestamp = timestamp
-  end
-
-  def mark_feed_as_read
-    @repo
-      .fetch_unread_for_feed_by_timestamp(@feed_id, @timestamp)
+module MarkFeedAsRead
+  def self.call(feed_id, timestamp)
+    StoryRepository
+      .fetch_unread_for_feed_by_timestamp(feed_id, timestamp)
       .update_all(is_read: true)
   end
 end
