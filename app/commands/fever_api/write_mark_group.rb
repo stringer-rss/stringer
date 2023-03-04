36e182e682c8b9965502fe002eeb80e5a7e82986
Robert Fletcher <lobatifricha@gmail.com>
2023-03-04 09:54:25 -0800

    Callable: make WriteMarkGroup a callable module

--- app/commands/fever_api/write_mark_group.rb
+++ app/commands/fever_api/write_mark_group.rb
@@ -1,18 +1,10 @@
 # frozen_string_literal: true
 
 module FeverAPI
-  class WriteMarkGroup
+  module WriteMarkGroup
     def self.call(params)
-      new.call(params)
-    end
-
-    def initialize(options = {})
-      @marker_class = options.fetch(:marker_class) { MarkGroupAsRead }
-    end
-
-    def call(params = {})
       if params[:mark] == "group"
-        @marker_class.new(params[:id], params[:before]).mark_group_as_read
+        MarkGroupAsRead.new(params[:id], params[:before]).mark_group_as_read
       end
 
       {}
