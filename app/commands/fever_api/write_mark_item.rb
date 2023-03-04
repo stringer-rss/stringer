9f123df4f593d6477c615e9b02980a6dab9e68bf
Robert Fletcher <lobatifricha@gmail.com>
2023-03-04 10:41:24 -0800

    Callable: make WriteMarkItem a callable module

--- app/commands/fever_api/write_mark_item.rb
+++ app/commands/fever_api/write_mark_item.rb
@@ -1,39 +1,27 @@
 # frozen_string_literal: true
 
 module FeverAPI
-  class WriteMarkItem
-    def self.call(params)
-      new.call(params)
-    end
+  module WriteMarkItem
+    class << self
+      def call(params)
+        mark_item_as(params[:id], params[:as]) if params[:mark] == "item"
 
-    def initialize(options = {})
-      @read_marker_class = options.fetch(:read_marker_class) { MarkAsRead }
-      @unread_marker_class =
-        options.fetch(:unread_marker_class) { MarkAsUnread }
-      @starred_marker_class =
-        options.fetch(:starred_marker_class) { MarkAsStarred }
-      @unstarred_marker_class =
-        options.fetch(:unstarred_marker_class) { MarkAsUnstarred }
-    end
+        {}
+      end
 
-    def call(params = {})
-      mark_item_as(params[:id], params[:as]) if params[:mark] == "item"
+      private
 
-      {}
-    end
-
-    private
-
-    def mark_item_as(id, mark_as)
-      case mark_as
-      when "read"
-        @read_marker_class.call(id)
-      when "unread"
-        @unread_marker_class.call(id)
-      when "saved"
-        @starred_marker_class.call(id)
-      when "unsaved"
-        @unstarred_marker_class.call(id)
+      def mark_item_as(id, mark_as)
+        case mark_as
+        when "read"
+          MarkAsRead.call(id)
+        when "unread"
+          MarkAsUnread.call(id)
+        when "saved"
+          MarkAsStarred.call(id)
+        when "unsaved"
+          MarkAsUnstarred.call(id)
+        end
       end
     end
   end
