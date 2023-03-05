409fc023ceec5e0071009e5264e5e63885d5c435
Robert Fletcher <lobatifricha@gmail.com>
2023-03-05 09:40:47 -0800

    Callable: make CreateUser a callable module (#964)

--- app/commands/users/create_user.rb
+++ app/commands/users/create_user.rb
@@ -1,16 +1,8 @@
 # frozen_string_literal: true
 
-class CreateUser
+module CreateUser
   def self.call(password)
-    new.call(password)
-  end
-
-  def initialize(repository = User)
-    @repo = repository
-  end
-
-  def call(password)
-    @repo.create(
+    User.create(
       username: "stringer",
       password:,
       password_confirmation: password
