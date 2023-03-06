4b94a6840e3025537da000f8e578d5ee3b160fba
Robert Fletcher <lobatifricha@gmail.com>
2023-03-05 20:08:59 -0800

    Callable: make OpmlParser a callable module

--- app/utils/opml_parser.rb
+++ app/utils/opml_parser.rb
@@ -2,45 +2,47 @@
 
 require "nokogiri"
 
-class OpmlParser
-  def parse_feeds(contents)
-    feeds_with_groups = Hash.new { |h, k| h[k] = [] }
+module OpmlParser
+  class << self
+    def call(contents)
+      feeds_with_groups = Hash.new { |h, k| h[k] = [] }
 
-    outlines_in(contents).each do |outline|
-      if outline_is_group?(outline)
-        group_name = extract_name(outline.attributes).value
-        feeds = outline.xpath("./outline")
-      else # it's a top-level feed, which means it's a feed without group
-        group_name = "Ungrouped"
-        feeds = [outline]
+      outlines_in(contents).each do |outline|
+        if outline_is_group?(outline)
+          group_name = extract_name(outline.attributes).value
+          feeds = outline.xpath("./outline")
+        else # it's a top-level feed, which means it's a feed without group
+          group_name = "Ungrouped"
+          feeds = [outline]
+        end
+
+        feeds.each do |feed|
+          feeds_with_groups[group_name] << feed_to_hash(feed)
+        end
       end
 
-      feeds.each do |feed|
-        feeds_with_groups[group_name] << feed_to_hash(feed)
-      end
+      feeds_with_groups
     end
 
-    feeds_with_groups
-  end
+    private
 
-  private
+    def outlines_in(contents)
+      Nokogiri.XML(contents).xpath("//body/outline")
+    end
 
-  def outlines_in(contents)
-    Nokogiri.XML(contents).xpath("//body/outline")
-  end
+    def outline_is_group?(outline)
+      outline.attributes["xmlUrl"].nil?
+    end
 
-  def outline_is_group?(outline)
-    outline.attributes["xmlUrl"].nil?
-  end
+    def extract_name(attributes)
+      attributes["title"] || attributes["text"]
+    end
 
-  def extract_name(attributes)
-    attributes["title"] || attributes["text"]
-  end
-
-  def feed_to_hash(feed)
-    {
-      name: extract_name(feed.attributes).value,
-      url: feed.attributes["xmlUrl"].value
-    }
+    def feed_to_hash(feed)
+      {
+        name: extract_name(feed.attributes).value,
+        url: feed.attributes["xmlUrl"].value
+      }
+    end
   end
 end
