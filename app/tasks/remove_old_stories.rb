9b968f2f39acb19d110a9e32ca123041847bd36e
Robert Fletcher <lobatifricha@gmail.com>
2023-03-06 19:26:26 -0800

    Callable: make RemoveOldStories a callable module

--- app/tasks/remove_old_stories.rb
+++ app/tasks/remove_old_stories.rb
@@ -1,19 +1,23 @@
 # frozen_string_literal: true
 
-class RemoveOldStories
-  def self.remove!(number_of_days)
-    stories = old_stories(number_of_days)
-    feeds = pruned_feeds(stories)
+module RemoveOldStories
+  class << self
+    def call(number_of_days)
+      stories = old_stories(number_of_days)
+      feeds = pruned_feeds(stories)
 
-    stories.delete_all
-    feeds.each { |feed| FeedRepository.update_last_fetched(feed, Time.now) }
-  end
+      stories.delete_all
+      feeds.each { |feed| FeedRepository.update_last_fetched(feed, Time.now) }
+    end
 
-  def self.old_stories(number_of_days)
-    StoryRepository.unstarred_read_stories_older_than(number_of_days)
-  end
+    private
 
-  def self.pruned_feeds(stories)
-    FeedRepository.fetch_by_ids(stories.map(&:feed_id))
+    def old_stories(number_of_days)
+      StoryRepository.unstarred_read_stories_older_than(number_of_days)
+    end
+
+    def pruned_feeds(stories)
+      FeedRepository.fetch_by_ids(stories.map(&:feed_id))
+    end
   end
 end
--- spec/tasks/remove_old_stories_spec.rb
+++ spec/tasks/remove_old_stories_spec.rb
@@ -1,51 +1,49 @@
 # frozen_string_literal: true
 
 RSpec.describe RemoveOldStories do
-  describe ".remove!" do
-    let(:stories_mock) do
-      stories = double("stories")
-      allow(stories).to receive(:delete_all)
-      stories
-    end
+  let(:stories_mock) do
+    stories = double("stories")
+    allow(stories).to receive(:delete_all)
+    stories
+  end
 
-    it "passes along the number of days to the story repository query" do
-      allow(described_class).to receive(:pruned_feeds).and_return([])
+  it "passes along the number of days to the story repository query" do
+    allow(described_class).to receive(:pruned_feeds).and_return([])
 
-      expect(StoryRepository).to receive(:unstarred_read_stories_older_than)
-        .with(7).and_return(stories_mock)
+    expect(StoryRepository).to receive(:unstarred_read_stories_older_than)
+      .with(7).and_return(stories_mock)
 
-      described_class.remove!(7)
-    end
+    described_class.call(7)
+  end
 
-    it "requests deletion of all old stories" do
-      allow(described_class).to receive(:pruned_feeds).and_return([])
-      allow(StoryRepository)
-        .to receive(:unstarred_read_stories_older_than) { stories_mock }
+  it "requests deletion of all old stories" do
+    allow(described_class).to receive(:pruned_feeds).and_return([])
+    allow(StoryRepository)
+      .to receive(:unstarred_read_stories_older_than) { stories_mock }
 
-      expect(stories_mock).to receive(:delete_all)
+    expect(stories_mock).to receive(:delete_all)
 
-      described_class.remove!(11)
-    end
+    described_class.call(11)
+  end
 
-    it "fetches affected feeds by id" do
-      stories = create_list(:story, 3, :read, created_at: 1.month.ago)
+  it "fetches affected feeds by id" do
+    stories = create_list(:story, 3, :read, created_at: 1.month.ago)
 
-      described_class.remove!(13)
+    described_class.call(13)
 
-      expect(Story.where(id: stories.map(&:id))).to be_empty
-    end
+    expect(Story.where(id: stories.map(&:id))).to be_empty
+  end
 
-    it "updates last_fetched on affected feeds" do
-      feeds = [double("feed a"), double("feed b")]
-      allow(described_class).to receive(:pruned_feeds) { feeds }
-      allow(described_class).to receive(:old_stories) { stories_mock }
+  it "updates last_fetched on affected feeds" do
+    feeds = [double("feed a"), double("feed b")]
+    allow(described_class).to receive(:pruned_feeds) { feeds }
+    allow(described_class).to receive(:old_stories) { stories_mock }
 
-      expect(FeedRepository)
-        .to receive(:update_last_fetched).with(feeds.first, anything)
-      expect(FeedRepository)
-        .to receive(:update_last_fetched).with(feeds.last, anything)
+    expect(FeedRepository)
+      .to receive(:update_last_fetched).with(feeds.first, anything)
+    expect(FeedRepository)
+      .to receive(:update_last_fetched).with(feeds.last, anything)
 
-      described_class.remove!(13)
-    end
+    described_class.call(13)
   end
 end
