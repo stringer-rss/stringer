7aaf343bbf3214a844a55cc1ab0e3775916248a0
Robert Fletcher <lobatifricha@gmail.com>
2023-03-10 21:24:12 -0800

    CodeQuality: remove repo arg from AddNewFeed

--- app/commands/feed/add_new_feed.rb
+++ app/commands/feed/add_new_feed.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 module AddNewFeed
-  def self.call(url, repo = Feed, user:)
+  def self.call(url, user:)
     result = FeedDiscovery.call(url)
     return false unless result
 
     name = ContentSanitizer.call(result.title.presence || result.feed_url)
 
-    repo.create(name:, user:, url: result.feed_url, last_fetched: 1.day.ago)
+    Feed.create(name:, user:, url: result.feed_url, last_fetched: 1.day.ago)
   end
 end
