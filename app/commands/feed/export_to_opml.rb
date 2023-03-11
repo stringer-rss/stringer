3ea6b9763fc289019bceae70caf7669311477cce
Robert Fletcher <lobatifricha@gmail.com>
2023-03-10 21:44:34 -0800

    RuboCop: fix method length in ExportToOpml

--- app/commands/feed/export_to_opml.rb
+++ app/commands/feed/export_to_opml.rb
@@ -1,24 +1,28 @@
 # frozen_string_literal: true
 
 module ExportToOpml
-  def self.call(feeds)
-    builder =
-      Nokogiri::XML::Builder.new do |xml|
-        xml.opml(version: "1.0") do
-          xml.head { xml.title("Feeds from Stringer") }
-          xml.body do
-            feeds.each do |feed|
-              xml.outline(
-                text: feed.name,
-                title: feed.name,
-                type: "rss",
-                xmlUrl: feed.url
-              )
-            end
+  class << self
+    def call(feeds)
+      builder =
+        Nokogiri::XML::Builder.new do |xml|
+          xml.opml(version: "1.0") do
+            xml.head { xml.title("Feeds from Stringer") }
+            xml.body { feeds.each { |feed| feed_outline(xml, feed) } }
           end
         end
-      end
 
-    builder.to_xml
+      builder.to_xml
+    end
+
+    private
+
+    def feed_outline(xml, feed)
+      xml.outline(
+        text: feed.name,
+        title: feed.name,
+        type: "rss",
+        xmlUrl: feed.url
+      )
+    end
   end
 end
