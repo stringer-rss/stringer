512b563d87bf0ef05e9eebc3f14061b44fb61e27
Robert Fletcher <lobatifricha@gmail.com>
2023-03-10 21:24:18 -0800

    RuboCop: fix module nesting (#984)
    Depending on the test order, it might not find `API_VERSION`.

---
+++ app/commands/fever_api.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module FeverAPI
+  API_VERSION = 3
+
+  PARAMS = [
+    :as,
+    :before,
+    :favicons,
+    :feeds,
+    :groups,
+    :id,
+    :items,
+    :links,
+    :mark,
+    :saved_item_ids,
+    :since_id,
+    :unread_item_ids,
+    :with_ids
+  ].freeze
+end
