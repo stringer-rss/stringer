d2e4927d446397e6f0755d970248691abe085fc1
Robert Fletcher <lobatifricha@gmail.com>
2023-03-10 21:37:57 -0800

    CodeQuality: remove client arg from FeedDiscovery

--- app/utils/feed_discovery.rb
+++ app/utils/feed_discovery.rb
@@ -2,19 +2,19 @@
 
 module FeedDiscovery
   class << self
-    def call(url, client = HTTParty)
-      get_feed_for_url(url, client) do
+    def call(url)
+      get_feed_for_url(url) do
         urls = Feedbag.find(url)
         return false if urls.empty?
 
-        get_feed_for_url(urls.first, client) { return false }
+        get_feed_for_url(urls.first) { return false }
       end
     end
 
     private
 
-    def get_feed_for_url(url, client)
-      response = client.get(url).to_s
+    def get_feed_for_url(url)
+      response = HTTParty.get(url).to_s
       feed = Feedjira.parse(response)
       feed.feed_url ||= url
       feed
