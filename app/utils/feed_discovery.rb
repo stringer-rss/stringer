773a0d02b0b818676d834d4c5bae19741f91f6c0
Robert Fletcher <lobatifricha@gmail.com>
2023-03-10 21:35:44 -0800

    CodeQuality: remove parser arg from FeedDiscovery

--- app/utils/feed_discovery.rb
+++ app/utils/feed_discovery.rb
@@ -2,20 +2,20 @@
 
 module FeedDiscovery
   class << self
-    def call(url, parser = Feedjira, client = HTTParty)
-      get_feed_for_url(url, parser, client) do
+    def call(url, client = HTTParty)
+      get_feed_for_url(url, client) do
         urls = Feedbag.find(url)
         return false if urls.empty?
 
-        get_feed_for_url(urls.first, parser, client) { return false }
+        get_feed_for_url(urls.first, client) { return false }
       end
     end
 
     private
 
-    def get_feed_for_url(url, parser, client)
+    def get_feed_for_url(url, client)
       response = client.get(url).to_s
-      feed = parser.parse(response)
+      feed = Feedjira.parse(response)
       feed.feed_url ||= url
       feed
     rescue StandardError
