02865577375d4af471a683ac3fba5717f1b04cf8
Robert Fletcher <lobatifricha@gmail.com>
2023-03-18 10:51:15 -0700

    MultiUsers: pass authorization to WriteMarkItem (#1013)

--- spec/commands/fever_api/write_mark_item_spec.rb
+++ spec/commands/fever_api/write_mark_item_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe FeverAPI::WriteMarkItem do
   context "when as: 'read'" do
     it "marks the story as read" do
       story = create(:story, :unread)
+      authorization = Authorization.new(story.user)
+      params = { authorization:, mark: "item", as: "read", id: story.id }
 
-      expect { subject.call(mark: "item", as: "read", id: story.id) }
+      expect { subject.call(**params) }
         .to change_record(story, :is_read).from(false).to(true)
     end
   end
@@ -13,8 +15,10 @@ RSpec.describe FeverAPI::WriteMarkItem do
   context "when as: 'unread'" do
     it "marks the story as unread" do
       story = create(:story, :read)
+      authorization = Authorization.new(story.user)
+      params = { authorization:, mark: "item", as: "unread", id: story.id }
 
-      expect { subject.call(mark: "item", as: "unread", id: story.id) }
+      expect { subject.call(**params) }
         .to change_record(story, :is_read).from(true).to(false)
     end
   end
@@ -22,8 +26,10 @@ RSpec.describe FeverAPI::WriteMarkItem do
   context "when as: 'saved'" do
     it "marks the story as starred" do
       story = create(:story)
+      authorization = Authorization.new(story.user)
+      params = { authorization:, mark: "item", as: "saved", id: story.id }
 
-      expect { subject.call(mark: "item", as: "saved", id: story.id) }
+      expect { subject.call(**params) }
         .to change_record(story, :is_starred).from(false).to(true)
     end
   end
@@ -31,13 +37,17 @@ RSpec.describe FeverAPI::WriteMarkItem do
   context "when as: 'unsaved'" do
     it "marks the story as unstarred" do
       story = create(:story, :starred)
+      authorization = Authorization.new(story.user)
+      params = { authorization:, mark: "item", as: "unsaved", id: story.id }
 
-      expect { subject.call(mark: "item", as: "unsaved", id: story.id) }
+      expect { subject.call(**params) }
         .to change_record(story, :is_starred).from(true).to(false)
     end
   end
 
   it "returns an empty hash when :as is not present" do
-    expect(subject.call({ mark: "item" })).to eq({})
+    authorization = Authorization.new(default_user)
+
+    expect(subject.call(authorization:, mark: "item")).to eq({})
   end
 end
