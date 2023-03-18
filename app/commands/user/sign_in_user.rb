d6c1c50dfc498ecd6d134b7c1dd3a0fe3b776849
Robert Fletcher <lobatifricha@gmail.com>
2023-03-18 13:44:00 -0700

    MultiUsers: require username when logging in

--- app/commands/user/sign_in_user.rb
+++ app/commands/user/sign_in_user.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 module SignInUser
-  def self.call(submitted_password)
-    user = User.first
+  def self.call(username, submitted_password)
+    user = User.find_by(username:)
+    return unless user
+
     user_password = BCrypt::Password.new(user.password_digest)
 
     user if user_password == submitted_password
--- spec/commands/user/sign_in_user_spec.rb
+++ spec/commands/user/sign_in_user_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe SignInUser do
   it "returns the user if the password is valid" do
-    result = described_class.call(default_user.password)
+    result = described_class.call(default_user.username, default_user.password)
 
     expect(result).to eq(default_user)
   end
@@ -10,7 +10,13 @@ RSpec.describe SignInUser do
   it "returns nil if password is invalid" do
     create(:user)
 
-    result = described_class.call("not-the-pw")
+    result = described_class.call(default_user.username, "not-the-pw")
+
+    expect(result).to be_nil
+  end
+
+  it "returns nil if the user does not exist" do
+    result = described_class.call("not-the-username", "not-the-pw")
 
     expect(result).to be_nil
   end
