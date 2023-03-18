ca4782dde59456e845a1160b4508382be323cfef
Robert Fletcher <lobatifricha@gmail.com>
2023-03-18 11:19:31 -0700

    MultiUsers: pass authorization to FeverAPI::Authentication (#1014)

--- app/commands/fever_api/authentication.rb
+++ app/commands/fever_api/authentication.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 module FeverAPI::Authentication
-  def self.call(_params)
-    last_refreshed_on_time = (Feed.maximum(:last_fetched) || 0).to_i
+  def self.call(authorization:, **_params)
+    feeds = authorization.scope(Feed)
+    last_refreshed_on_time = (feeds.maximum(:last_fetched) || 0).to_i
 
     { auth: 1, last_refreshed_on_time: }
   end
