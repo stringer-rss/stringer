9c466db2378c5d825f03de1bdd3fb965c4b8d3b1
Robert Fletcher <lobatifricha@gmail.com>
2023-03-18 10:23:40 -0700

    MultiUsers: pass authorization to WriteMarkGroup

--- app/commands/fever_api/write_mark_group.rb
+++ app/commands/fever_api/write_mark_group.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 module FeverAPI::WriteMarkGroup
-  def self.call(params)
+  def self.call(authorization:, **params)
     if params[:mark] == "group"
+      authorization.check(Group.find(params[:id]))
       MarkGroupAsRead.call(params[:id], params[:before])
     end
 
--- spec/commands/fever_api/write_mark_group_spec.rb
+++ spec/commands/fever_api/write_mark_group_spec.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.describe FeverAPI::WriteMarkGroup do
+  def authorization
+    Authorization.new(default_user)
+  end
+
   it "marks the group stories as read before the given timestamp" do
     story = create(:story, :unread, :with_group, created_at: 1.week.ago)
     before = 1.day.ago
     id = story.group_id
 
-    expect { described_class.call(mark: "group", id:, before:) }
+    expect { described_class.call(authorization:, mark: "group", id:, before:) }
       .to change_record(story, :is_read).from(false).to(true)
   end
 
   it "returns an empty hash otherwise" do
-    expect(described_class.call({})).to eq({})
+    expect(described_class.call(authorization:)).to eq({})
   end
 end
