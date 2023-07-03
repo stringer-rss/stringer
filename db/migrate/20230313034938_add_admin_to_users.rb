7de9d5bc43975c75d9e06d2150672b67806a5998
Robert Fletcher <lobatifricha@gmail.com>
2023-07-03 10:56:45 -0700

    repair migrations
    **What**
    
    This makes a couple of updates to the migrations based on [this
    conversation][is]:
    
    * Rename `Api` -> `API`. This is expected by Rails inflections.
    * Add conditional checks to migrations that look for a user. If a person
      hasn't set up an account yet, the migrations should still run
      gracefully.
    
    [is]: https://github.com/stringer-rss/stringer/issues/1046

--- db/migrate/20230313034938_add_admin_to_users.rb
+++ db/migrate/20230313034938_add_admin_to_users.rb
@@ -4,7 +4,7 @@ class AddAdminToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :admin, :boolean
 
-    User.first.update!(admin: true)
+    User.first.update!(admin: true) if User.any?
 
     change_column_null :users, :admin, false
   end
