1d7448ca8d8f487baafa3cc9c9c78b8b0648b607
Robert Fletcher <lobatifricha@gmail.com>
2023-07-20 09:44:03 -0700

    Rails: adjust username migration
    Should hopefully fix [this issue][is] where an error is thrown during
    the migration. I also realized there is a validation issue for the api
    key, so changed to `update_columns` to bypass migrations.
    
    [is]: https://github.com/stringer-rss/stringer/issues/1062

--- db/migrate/20230223231930_add_username_to_users.rb
+++ db/migrate/20230223231930_add_username_to_users.rb
@@ -4,7 +4,8 @@ class AddUsernameToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :username, :string
     add_index :users, :username, unique: true
-    User.first.update!(username: "stringer") if User.any?
+    ActiveRecord::Base.clear_cache!
+    User.first.update_columns(username: "stringer") if User.any?
     change_column_null :users, :username, false
   end
 end
