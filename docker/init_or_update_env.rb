d5883ad8e6aa7b22f64b2f7e3cb182fb2a20f46e
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2023-10-26 21:42:16 -0500

    use has_key instead of present

--- docker/init_or_update_env.rb
+++ docker/init_or_update_env.rb
@@ -4,10 +4,10 @@ required_env = {
   "ENCRYPTION_DETERMINISTIC_KEY" => `openssl rand -hex 64`.strip,
   "ENCRYPTION_KEY_DERIVATION_SALT" => `openssl rand -hex 64`.strip,
   # ternary operators ensure that we can set the database url if it does not exist
-  "POSTGRES_USER" => ENV["POSTGRES_USER"] ? ENV["POSTGRES_USER"] : "stringer",
-  "POSTGRES_PASSWORD" => ENV["POSTGRES_PASSWORD"] ? ENV["POSTGRES_PASSWORD"] : `openssl rand -hex 32`.strip,
-  "POSTGRES_HOSTNAME" => ENV["POSTGRES_HOSTNAME"] ? ENV["POSTGRES_HOSTNAME"] : "stringer-postgres",
-  "POSTGRES_DB" => ENV["POSTGRES_DB"] ? ENV["POSTGRES_DB"] : "stringer",
+  "POSTGRES_USER" => ENV.has_key?("POSTGRES_USER") ? ENV["POSTGRES_USER"] : "stringer",
+  "POSTGRES_PASSWORD" => ENV.has_key?("POSTGRES_PASSWORD") ? ENV["POSTGRES_PASSWORD"] : `openssl rand -hex 32`.strip,
+  "POSTGRES_HOSTNAME" => ENV.has_key?("POSTGRES_HOSTNAME") ? ENV["POSTGRES_HOSTNAME"] : "stringer-postgres",
+  "POSTGRES_DB" => ENV.has_key?("POSTGRES_DB") ? ENV["POSTGRES_DB"] : "stringer",
   "FETCH_FEEDS_CRON" => "*/5 * * * *",
   "CLEANUP_CRON" => "0 0 * * *",
 }
@@ -15,7 +15,7 @@ required_env = {
 required_env["DATABASE_URL"] = "postgres://#{required_env['POSTGRES_USER']}:#{required_env['POSTGRES_PASSWORD']}@#{required_env['POSTGRES_HOSTNAME']}/#{required_env['POSTGRES_DB']}"
 
 required_env.each do |key, value|
-  next if ENV[key].present?
+  next if ENV.has_key?(key)
 
   File.open("/.env", "a") { |file| file << "#{key}=#{value}\n"
 end
