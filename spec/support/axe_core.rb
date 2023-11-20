aa944a82984f69c4a0022825aa707624a72ad44e
Robert Fletcher <lobatifricha@gmail.com>
2023-11-20 14:52:21 -0800

    A11y: add accessibility overrides for system tests
    **What**
    
    This adds the `axe-core-rspec` gem and overrides the Capybara methods
    `visit` and `click_on` to run accessibility checks after each click or
    visit.
    
    **Notes**
    
    This doesn't currently have any affordances to reduce test flake. `axe`
    is likely to end up running on partially loaded pages which could lead
    to unexpected test failures. I'm planning on running the build on repeat
    and fixing the errors as they come up.
    
    I might also add some sort of "expect_text" parameter to require *all*
    calls to expect some sort of text on the resulting page. I'm hesitant to
    vary too far from the Capbyara API, though.

---
+++ spec/support/axe_core.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "axe-rspec"
+
+module AccessibilityOverrides
+  A11Y_SKIP = [
+    "aria-required-children",
+    "color-contrast",
+    "html-has-lang",
+    "label-title-only",
+    "landmark-one-main",
+    "link-name",
+    "page-has-heading-one",
+    "region"
+  ].freeze
+
+  def visit(*, accessible: true, a11y_skip: A11Y_SKIP)
+    page.visit(*)
+
+    yield if block_given?
+
+    check_accessibility(a11y_skip:) if accessible
+  end
+
+  def click_on(*, a11y_skip: A11Y_SKIP, **)
+    page.click_on(*, **)
+
+    yield if block_given?
+
+    check_accessibility(a11y_skip:)
+  end
+
+  def check_accessibility(a11y_skip:)
+    within_window(current_window) do
+      expect(page).to have_css("div")
+      expect(page).to be_axe_clean.skipping(*a11y_skip)
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include(AccessibilityOverrides, type: :system)
+end
