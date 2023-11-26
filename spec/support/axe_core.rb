1fe6848082c53db390b01d2c3a4913488de16965
Rob Young <rhyoung214@gmail.com>
2023-11-26 14:58:36 -0800

    Updated css/views for color-contrast accessibility
    Description:
    Updated colors to meet color-contrast accessibility requirements. Also
    included a PATH_SKIP constant to avoid testing accessibility on good_job
    and mocha test dashboards.
    One remaining color-contrast failure due to read feeds light gray color
    
    application.css, flat-ui-no-icons.css, _flash.html.erb
     - Lightened orange color for main background
     - Darkened orange color for text, buttons, footer
     - Darkened green color for logged in user link, and success alerts
     - Darkened gray color for action bar buttons
    
    axe_core.rb
     - Added paths array to skip accessibility testing for jobs dashboard
       and mocha test

--- spec/support/axe_core.rb
+++ spec/support/axe_core.rb
@@ -14,9 +14,11 @@ module AccessibilityOverrides
     "region"
   ].freeze
 
+  PATH_SKIP = ["/admin/good_job/jobs", "/test"].freeze
+
   def visit(*, accessible: true, a11y_skip: A11Y_SKIP)
     page.visit(*)
-
+    accessible = false if PATH_SKIP.include?(page.current_path)
     yield if block_given?
 
     check_accessibility(a11y_skip:) if accessible
@@ -32,6 +34,7 @@ module AccessibilityOverrides
 
   def check_accessibility(a11y_skip:)
     within_window(current_window) do
+      puts(page.current_path)
       expect(page).to have_css("div")
       expect(page).to be_axe_clean.skipping(*a11y_skip)
     end
