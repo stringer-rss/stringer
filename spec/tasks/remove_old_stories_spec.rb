5ba729c8989576a0443614d99a51d8a1c57d030e
RobBoothAppDev21 <91844645+RobBoothAppDev21@users.noreply.github.com>
2024-03-18 22:03:24 -0700

    FIX: Remove memoization from utils and tasks specs (#1175)
    Co-authored-by: Rob Young <rhyoung214@gmail.com>

--- spec/tasks/remove_old_stories_spec.rb
+++ spec/tasks/remove_old_stories_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe RemoveOldStories do
-  let(:stories_mock) do
+  def create_stories
     stories = double("stories")
     allow(stories).to receive(:delete_all)
     stories
@@ -11,17 +11,18 @@ RSpec.describe RemoveOldStories do
     allow(described_class).to receive(:pruned_feeds).and_return([])
 
     expect(StoryRepository).to receive(:unstarred_read_stories_older_than)
-      .with(7).and_return(stories_mock)
+      .with(7).and_return(create_stories)
 
     described_class.call(7)
   end
 
   it "requests deletion of all old stories" do
     allow(described_class).to receive(:pruned_feeds).and_return([])
+    mocked_stories = create_stories
     allow(StoryRepository)
-      .to receive(:unstarred_read_stories_older_than) { stories_mock }
+      .to receive(:unstarred_read_stories_older_than) { mocked_stories }
 
-    expect(stories_mock).to receive(:delete_all)
+    expect(mocked_stories).to receive(:delete_all)
 
     described_class.call(11)
   end
@@ -37,7 +38,7 @@ RSpec.describe RemoveOldStories do
   it "updates last_fetched on affected feeds" do
     feeds = [double("feed a"), double("feed b")]
     allow(described_class).to receive(:pruned_feeds) { feeds }
-    allow(described_class).to receive(:old_stories) { stories_mock }
+    allow(described_class).to receive(:old_stories) { create_stories }
 
     expect(FeedRepository)
       .to receive(:update_last_fetched).with(feeds.first, anything)
