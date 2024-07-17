2402696dcb1ff33b1a8f160019a9dbc8dcaa5715
Ostap Brehin <osbre@protonmail.com>
2024-07-17 12:29:25 +0100

    Refactor migrations to not use `ActiveRecord` for compatibility

--- db/migrate/20230223045525_add_null_false_to_associations.rb
+++ db/migrate/20230223045525_add_null_false_to_associations.rb
@@ -2,13 +2,21 @@
 
 class AddNullFalseToAssociations < ActiveRecord::Migration[7.0]
   def change
-    if User.any?
-      Feed.where(user: nil).update_all(user_id: User.first.id)
-      Group.where(user: nil).update_all(user_id: User.first.id)
-    end
+    update_null_foreign_keys
 
     change_column_null :feeds, :user_id, false
     change_column_null :groups, :user_id, false
     change_column_null :stories, :feed_id, false
+  end
+
+  private
+
+  def update_null_foreign_keys
+    first_user_id = connection.select_value("SELECT id FROM users ORDER BY id LIMIT 1")
+
+    return unless first_user_id
+
+    connection.update("UPDATE feeds SET user_id = #{first_user_id} WHERE user_id IS NULL")
+    connection.update("UPDATE groups SET user_id = #{first_user_id} WHERE user_id IS NULL")
   end
 end
--- db/migrate/20230313034938_add_admin_to_users.rb
+++ db/migrate/20230313034938_add_admin_to_users.rb
@@ -2,10 +2,20 @@
 
 class AddAdminToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :admin, :boolean
+    add_column :users, :admin, :boolean, default: false
 
-    User.first.update!(admin: true) if User.any?
+    set_first_user_as_admin
 
     change_column_null :users, :admin, false
+  end
+
+  private
+
+  def set_first_user_as_admin
+    first_user_id = connection.select_value("SELECT id FROM users ORDER BY id LIMIT 1")
+
+    return unless first_user_id
+
+    connection.update("UPDATE users SET admin = TRUE WHERE id = #{first_user_id}")
   end
 end
