fda6fad1eef64f40f561061dd338e9873fb30c61
Ostap Brehin <osbre@protonmail.com>
2024-07-19 08:42:01 +0100

    Refactor migrations to not use `ActiveRecord` for compatibility (#1212)

--- db/migrate/20230223231930_add_username_to_users.rb
+++ db/migrate/20230223231930_add_username_to_users.rb
@@ -4,8 +4,19 @@ class AddUsernameToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :username, :string
     add_index :users, :username, unique: true
-    ActiveRecord::Base.clear_cache!
-    User.first.update_columns(username: "stringer") if User.any?
+
+    set_default_username
+
     change_column_null :users, :username, false
+  end
+
+  private
+
+  def set_default_username
+    first_user_id = connection.select_value("SELECT id FROM users ORDER BY id LIMIT 1")
+
+    return unless first_user_id
+
+    connection.update("UPDATE users SET username = 'stringer' WHERE id = #{first_user_id}")
   end
 end
--- db/migrate/20230301024452_encrypt_api_key.rb
+++ db/migrate/20230301024452_encrypt_api_key.rb
@@ -4,11 +4,20 @@ class EncryptAPIKey < ActiveRecord::Migration[7.0]
   def change
     ActiveRecord::Encryption.config.support_unencrypted_data = true
 
-    User.find_each(&:encrypt)
+    encrypt_api_keys
 
     ActiveRecord::Encryption.config.support_unencrypted_data = false
 
     change_column_null :users, :api_key, false
     add_index :users, :api_key, unique: true
+  end
+
+  private
+
+  def encrypt_api_keys
+    connection.select_all("SELECT id, api_key FROM users").each do |user|
+      encrypted_api_key = ActiveRecord::Encryption.encrypt(user["api_key"])
+      connection.update("UPDATE users SET api_key = #{connection.quote(encrypted_api_key)} WHERE id = #{user['id']}")
+    end
   end
 end
