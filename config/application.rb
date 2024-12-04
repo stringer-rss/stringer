0ef18cd5ef2c657f863f15891b52fe585246a3ef
Robert Fletcher <lobatifricha@gmail.com>
2024-12-03 19:36:29 -0800

    Rails8: update Rails config

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ end
 
 class Stringer::Application < Rails::Application
   # Initialize configuration defaults for originally generated Rails version.
-  config.load_defaults(7.1)
+  config.load_defaults(8.0)
 
   # Please, add to the `ignore` list any other `lib` subdirectories that do
   # not contain `.rb` files, or that should not be reloaded or eager loaded.
--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -7,5 +7,16 @@
 # information. See the ActiveSupport::ParameterFilter documentation for
 # supported notations and behaviors.
 Rails.application.config.filter_parameters += [
-  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
+  :passw,
+  :email,
+  :secret,
+  :token,
+  :_key,
+  :crypt,
+  :salt,
+  :certificate,
+  :otp,
+  :ssn,
+  :cvv,
+  :cvc
 ]
