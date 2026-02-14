35c583ffab644ec1008896423eb67ad8e4e14f72
Robert Fletcher <lobatifricha@gmail.com>
2026-02-13 17:51:03 -0800

    integrate Stylelint

---
+++ stylelint.config.mjs
@@ -0,0 +1,23 @@
+export default {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recess-order",
+    "./.stylelint_todo.yml",
+  ],
+  ignoreFiles: [
+    "app/assets/stylesheets/bootstrap-min.css",
+    "app/assets/stylesheets/flat-ui-no-icons.css",
+    "app/assets/stylesheets/font-awesome-min.css",
+    "app/assets/stylesheets/lato-fonts.css",
+    "app/assets/stylesheets/reenie-beanie-font.css",
+    "coverage/**",
+    "node_modules/**",
+  ],
+  plugins: ["stylelint-selector-bem-pattern"],
+  rules: {
+    "no-descending-specificity": null,
+    "plugin/selector-bem-pattern": {
+      preset: "bem",
+    },
+  },
+};
