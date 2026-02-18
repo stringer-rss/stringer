b4bb3199cccc2a666b559e1473edf908dffcf35a
Robert Fletcher <lobatifricha@gmail.com>
2026-02-18 13:53:56 -0800

    structure styles a bit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,12 +1,3 @@
 # frozen_string_literal: true
 
 # Be sure to restart your server when you modify this file.
-
-# Add additional assets to the asset load path.
-Rails.application.config.assets.paths += [
-  Rails.root.join("node_modules/bootstrap/dist/css"),
-  Rails.root.join("node_modules/@fontsource/lato/files"),
-  Rails.root.join("node_modules/@fontsource/reenie-beanie/files"),
-  Rails.root.join("node_modules/font-awesome/css"),
-  Rails.root.join("node_modules/font-awesome/fonts")
-]
