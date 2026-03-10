f994cc2b3a22f46aa6f3881ae38f6db127b1605d
Robert Fletcher <lobatifricha@gmail.com>
2026-03-10 10:36:44 -0700

    more system tests

--- spec/system/authentication_spec.rb
+++ spec/system/authentication_spec.rb
@@ -34,4 +34,36 @@ RSpec.describe "authentication" do
 
     expect(page).to have_current_path(login_path)
   end
+
+  it "redirects to login when accessing profile while logged out" do
+    visit(edit_profile_path)
+
+    expect(page).to have_current_path(login_path)
+  end
+
+  it "redirects to login when accessing new feed while logged out" do
+    visit(feeds_new_path)
+
+    expect(page).to have_current_path(login_path)
+  end
+
+  it "redirects to login when accessing feed import while logged out" do
+    visit(feeds_import_path)
+
+    expect(page).to have_current_path(login_path)
+  end
+
+  it "redirects to login when accessing feed export while logged out" do
+    visit(feeds_export_path)
+
+    expect(page).to have_current_path(login_path)
+  end
+
+  it "redirects to login when accessing feed edit while logged out" do
+    feed = create(:feed)
+
+    visit("/feeds/#{feed.id}/edit")
+
+    expect(page).to have_current_path(login_path)
+  end
 end
