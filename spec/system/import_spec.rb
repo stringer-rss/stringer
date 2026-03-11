d783540dbb412503b274ec60ad452796558241e0
Robert Fletcher <lobatifricha@gmail.com>
2026-03-10 20:13:44 -0700

    test group stuff (#1436)

--- spec/system/import_spec.rb
+++ spec/system/import_spec.rb
@@ -29,4 +29,40 @@ RSpec.describe "importing feeds" do
 
     expect(page).to have_content("We're getting you some stories to read")
   end
+
+  def import_grouped_opml
+    file_path = Rails.root.join("spec/fixtures/feeds.opml")
+    attach_file("opml_file", file_path, visible: false)
+    expect(page).to have_text("We're getting you some stories to read")
+  end
+
+  it "creates groups from the OPML structure" do
+    login_as(default_user)
+    visit(feeds_import_path)
+
+    import_grouped_opml
+
+    group_names = default_user.groups.pluck(:name)
+    expect(group_names).to include("Football News", "RoR")
+  end
+
+  it "assigns imported feeds to their groups" do
+    login_as(default_user)
+    visit(feeds_import_path)
+
+    import_grouped_opml
+
+    feed = default_user.feeds.find_by!(name: "TMW Football Transfer News")
+    expect(feed.group.name).to eq("Football News")
+  end
+
+  it "imports ungrouped feeds without a group" do
+    login_as(default_user)
+    visit(feeds_import_path)
+
+    import_grouped_opml
+
+    feed = default_user.feeds.find_by!(name: "Autoblog")
+    expect(feed.group).to be_nil
+  end
 end
